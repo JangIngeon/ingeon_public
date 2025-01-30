1fd63c449eb5657915c45bcdb50061b8fd323574
JangIngeon <wkddlsrjs@gmail.com>
2025-01-30 16:38:46 +0900

    Update fighting.py

--- fighting.py
+++ fighting.py
@@ -8,6 +8,11 @@ if st.button("확인"):
     if name:
         # 성을 제외한 이름 부분 추출
         first_name = name[1:]  # 첫 글자를 제외한 나머지
-        st.write(f"{first_name}아 힘내!")
+        
+        # 밝고 희망찬 폰트 스타일로 출력
+        st.markdown(f"""
+        <h1 style='color: #FF6F61; font-family: "Comic Sans MS", cursive, sans-serif; font-size: 60px; text-align: center;'> 
+        {first_name}아 힘내!</h1>
+        """, unsafe_allow_html=True)
     else:
         st.write("이름을 입력해주세요.")
