211f78435ef159478ddae9c0ab978110148f34bd
JangIngeon <wkddlsrjs@gmail.com>
2025-01-30 16:43:14 +0900

    Update fighting.py

--- fighting.py
+++ fighting.py
@@ -9,10 +9,13 @@ if st.button("확인"):
         # 성을 제외한 이름 부분 추출
         first_name = name[1:]  # 첫 글자를 제외한 나머지
         
-        # 파란색 글씨로 출력
+        # 파란색 글씨와 이미지 함께 출력
         st.markdown(f"""
         <h1 style='color: #1E90FF; font-family: "Comic Sans MS", cursive, sans-serif; font-size: 60px; text-align: center;'> 
         {first_name}아 힘내!</h1>
         """, unsafe_allow_html=True)
+        
+        # 이미지 출력
+        st.image("https://cdn.cashfeed.co.kr/attachments/03a1ab8e70.jpg", use_column_width=True)
     else:
         st.write("이름을 입력해주세요.")
