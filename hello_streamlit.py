e5dc9cf6c2fce94f5a092ef82d934105f2e41bbd
JangIngeon <wkddlsrjs@gmail.com>
2025-01-30 16:02:49 +0900

    Update hello_streamlit.py

--- hello_streamlit.py
+++ hello_streamlit.py
@@ -3,6 +3,10 @@ import streamlit as st
 # 이름 입력 받기
 name = st.text_input("이름을 입력하세요:")
 
-# 입력한 이름이 있으면 메시지 출력
-if name:
-    st.write(f"{name}은/는 바보입니다")
+# 확인 버튼
+if st.button("확인"):
+    if name:
+        st.markdown(f"<h1 style='color: red; font-size: 60px; text-align: center;'>당신은 바보입니다</h1>", unsafe_allow_html=True)
+    else:
+        st.write("이름을 입력해주세요.")
+
