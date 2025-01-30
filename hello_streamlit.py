a7f9e6be821e3f7436ff2be3b3888f34d30cc185
JangIngeon <wkddlsrjs@gmail.com>
2025-01-30 16:14:17 +0900

    Update hello_streamlit.py

--- hello_streamlit.py
+++ hello_streamlit.py
@@ -8,10 +8,11 @@ if st.button("확인"):
     if name:
         # 텍스트와 이미지 함께 출력
         st.markdown(f"""
-        <h1 style='color: red; font-size: 60px; text-align: center;'>당신은 바보입니다</h1>
+        <h1 style='color: red; font-size: 60px; text-align: center;'>쉬나 드십쇼</h1>
         <img src="https://img1.daumcdn.net/thumb/R1280x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/cnoC/image/-z3AZcHOGwkI_TnqZmiLn3kvzyQ" alt="Image" style="display: block; margin: 0 auto; width: 50%;">
         """, unsafe_allow_html=True)
     else:
         st.write("이름을 입력해주세요.")
+
 
 
